e081e651adeeee0cd0181db9f6f93c67c7da0445
yingshu0218 <yingshu0218@users.noreply.github.com>
2026-05-29 23:29:52 +0800

    bump formula to v1.1.0

--- Formula/brew-update-all.rb
+++ Formula/brew-update-all.rb
@@ -1,8 +1,8 @@
 class BrewUpdateAll < Formula
-  desc "逐个升级 Homebrew formulae 和 cask，支持交互选择或自动模式"
+  desc "逐个升级 Homebrew formulae 和 cask，支持 --greedy / 交互选择 / 自动模式"
   homepage "https://github.com/yingshu0218/homebrew-update-all"
-  url "https://github.com/yingshu0218/homebrew-update-all/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "5fb0efd164dbe752b36bc57c3751450aaa7eb8e26a4bca4233944c0fa0a1eace"
+  url "https://github.com/yingshu0218/homebrew-update-all/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "3a02d5ebfffd96d36c5b0389bb02b00ae868315f31b28eed2fa6718ac9a63842"
 
   def install
     bin.install "brew-ua"
