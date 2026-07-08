9bd897d0769110c5ee9199b9cdc6d0e735c1ad16
yingshu0218 <yingshu0218@users.noreply.github.com>
2026-07-08 07:44:20 +0000

    Bump version to v1.7.0

--- Formula/brew-update-all.rb
+++ Formula/brew-update-all.rb
@@ -1,8 +1,8 @@
 class BrewUpdateAll < Formula
   desc "逐个升级 Homebrew formulae 和 cask，支持包大小显示 / 下载速度 / 深度清理 / 更新进度"
   homepage "https://github.com/yingshu0218/homebrew-update-all"
-  url "https://github.com/yingshu0218/homebrew-update-all/archive/refs/tags/v1.6.5.tar.gz"
-  sha256 "959bf943de03cfe0533928f49a44aaec61c4651cd2f1d633aa53ca93a4edf73d"
+  url "https://github.com/yingshu0218/homebrew-update-all/archive/refs/tags/v1.7.0.tar.gz"
+  sha256 "2865152686c5694d15f33033707d40d26c9bd45c2fd109a0e4369539ec0d811f"
 
   def install
     bin.install "brew-ua"
